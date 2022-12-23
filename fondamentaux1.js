15a8dc3bc083b3a7e89834f93b7fb1d735525e42
romain_verhack <r.verhack@gmail.com>
2022-12-23 13:42:24 +0100

    MAJ fondamentaux1.js

--- fondamentaux1.js
+++ fondamentaux1.js
@@ -1,32 +1,26 @@
-/*Écrivez un programme JavaScript qui demande à l'utilisateur de saisir un nombre de noms.
+//1. Écrivez un programme JavaScript qui demande à l'utilisateur de saisir un nombre de noms.
+const numberOfName = prompt("Combien de noms souhaitez-vous inscrire ?");
 
-Utilisez une boucle pour demander à l'utilisateur de saisir chaque nom. Les noms saisis doivent être stockés dans un tableau.
-
-Concaténez plusieurs chaînes de caractères pour construire un message de confirmation contenant le nombre de noms saisis et la liste de ces noms. Si le nombre de noms est supérieur à 3, ajoutez une info supplémentaire au message (par exemple : "C'est beaucoup de noms !").
-
-Affichez le message de confirmation dans la console.
-
-Écrivez une fonction qui prend en paramètre un tableau de noms et qui affiche chacun de ces noms dans la console.
-Appelez la fonction avec le tableau de noms en argument.
-*/
-
-// saisir un nombre de noms : prompt()
-// boucle for, le nombre de noms, ajouter les noms dans un tableau. Creer le tableau avant
-// if nombre de noms < 4 afficher "vous avez ecrit nombre de noms : tableau"
-
-const numberOfName = prompt("Combien de noms souhaitez-vous inscrire ?")
-const names = []
-
+//1.1 Utilisez une boucle pour demander à l'utilisateur de saisir chaque nom. Les noms saisis doivent être stockés dans un tableau.
+const names = [];
 for (let i = 0; i < parseInt(numberOfName); i++) {
     names.push(prompt("Ecrivez un nom"))
 }
 
-if (parseInt(numberOfName) === 1) {
-    console.log(`Vous avez indiqué ${numberOfName} nom : ${names}`)
-} else if (1 < numberOfName < 4){
+// 2. Concaténez plusieurs chaînes de caractères pour construire un message de confirmation contenant le nombre de noms saisis et la liste de ces noms. Si le nombre de noms est supérieur à 3, ajoutez une info supplémentaire au message (par exemple : "C'est beaucoup de noms !").
+// 2.1 Affichez le message de confirmation dans la console.
+if (parseInt(numberOfName)>3) {
+    console.log(`Vous avez indiqué ${numberOfName} noms : ${names}, \nc'est beaucoup de noms !`)
+}else {
     console.log(`Vous avez indiqué ${numberOfName} noms : ${names}`)
-} else {
-    console.log(`Vous avez indiqué ${numberOfName} noms : ${names} \n C'est beaucoup de noms !`)
 }
 
+// 4. Écrivez une fonction qui prend en paramètre un tableau de noms et qui affiche chacun de ces noms dans la console.
+function showtab (tab) {
+    for (let i = 0; i < tab.length; i++) {
+        console.log(tab[i])  
+    }
+}
 
+// 5. Appelez la fonction avec le tableau de noms en argument.
+showtab(names)
